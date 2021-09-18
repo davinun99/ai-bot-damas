3393409296af1aa060426d03e4a54eda768c19df
matiaslopezs <matigls2@fpuna.edu.py>
2021-09-18 14:06:35 -0400

    se cambian las igualdades dobles a igualdades triples

--- src/classes/MinimaxPodaAlfaBeta.js
+++ src/classes/MinimaxPodaAlfaBeta.js
@@ -14,22 +14,39 @@ class MinimaxPodaAlfaBeta {
 
     jugar(){
         let index = 0;
-        while (this.tableroActual.calcularResultado() == JUEGO_INCONCLUSO) {
+        while (this.tableroActual.calcularResultado() === JUEGO_INCONCLUSO && index <100) { //limitamos a 200 turnos máximo
             index++;
         //for (let index = 0; index < 25; index++) {
-            this.tableroActual.dibujarTablero();
-            //movimiento del rival
-            console.log(index+") cantidad de fichas: "+this.tableroActual.cantFichasBlancas+" blancas y "+this.tableroActual.cantFichasNegras+" negras"); //mostramos al rival sus jugadas disponibles
-            console.log("se elegira una jugada random entre las disponibles ");
-            this.tableroActual.jugarRandom(this.rival);
-            //movimiento del minimax
-            this.movimientoElegido = []; //hacemos vacío el movimiento elegido
-            console.log("print minimax: "+this.maxValue(this.tableroActual,this.profundidadMax,this.alfa,this.beta));
-            console.log("juego: "+this.tableroActual.calcularResultado());
-            let {movimiento, puedeCapturar, fichasCapturadas, ficha} = this.movimientoElegido; //desarmamos el movimiento
-            let [movFila, movColumna] = movimiento; //ver valores del movimiento
-            console.log('mov elegido: ['+movFila+']['+movColumna+']');
-            this.tableroActual.jugar(this.movimientoElegido); //jugamos con el movimiento elegido que fue cargado en maxValue
+            //antes de jugar vemos las jugadas disponibles
+            /*
+            if (this.tableroActual.getAllJugadas(this.rival).length === 0) { //si el rival ya no tiene movimientos posibles pierde
+                console.log("el rival ha perdido");
+                break;
+            } else if(this.tableroActual.getAllJugadas(this.jugador).length === 0){ //si el jugador ya no tiene movimientos posibles pierde
+                console.log("minimax con poda ha perdido");
+                break;
+            }else{*/ //de lo contrario sigue el juego
+                this.tableroActual.dibujarTablero();
+
+                console.log('cant mov rival '+this.tableroActual.getAllJugadas(this.rival));
+                console.log('cant mov minim '+this.tableroActual.getAllJugadas(this.jugador));
+
+                //movimiento del rival
+                console.log(index+") cantidad de fichas: "+this.tableroActual.cantFichasBlancas+" blancas y "+this.tableroActual.cantFichasNegras+" negras"); //mostramos al rival sus jugadas disponibles
+                console.log("se elegira una jugada random entre las disponibles ");
+                this.tableroActual.jugarRandom(this.rival);
+                console.log('movimiento de rival');
+                this.tableroActual.dibujarTablero();
+                //movimiento del minimax
+                this.movimientoElegido = []; //hacemos vacío el movimiento elegido
+                console.log("print minimax: "+this.maxValue(this.tableroActual,this.profundidadMax,this.alfa,this.beta));
+                //console.log("juego: "+this.tableroActual.calcularResultado());
+                let {movimiento, puedeCapturar, fichasCapturadas, ficha} = this.movimientoElegido; //desarmamos el movimiento
+                let [movFila, movColumna] = movimiento; //ver valores del movimiento
+                console.log('mov elegido: ['+movFila+']['+movColumna+']');
+                this.tableroActual.jugar(this.movimientoElegido); //jugamos con el movimiento elegido que fue cargado en maxValue
+            //}
+            //this.tableroActual.calcularResultado();
         }
     }
 
@@ -70,32 +87,31 @@ class MinimaxPodaAlfaBeta {
                 return alfa;
             }
         }
-        console.log('profundidad: '+(4-profundidadMax)+' valor(beta):'+ beta);
+        //console.log('profundidad: '+(4-profundidadMax)+' valor(beta):'+ beta);
         return beta;
     }
 
     
     rewardJugada(tableroActual){ //función de reward de cada jugada que llega a las hojas del árbol
         let reward = 0;
-        if (tableroActual.calcularResultado() == GANAN_BLANCAS) { //si ganan las blancas
-            if (this.jugador == 2) { //si el jugador es blancas entonces gana
+        if (tableroActual.calcularResultado() === GANAN_BLANCAS) { //si ganan las blancas
+            if (this.jugador === 2) { //si el jugador es blancas entonces gana
                 reward = 100;
             } else { //pierde
                 reward = -100;
             }
-        } else if(tableroActual.calcularResultado() == GANAN_NEGRAS) { //si ganan las negras
-            if (this.jugador == 1) { //si el jugador es negras entonces gana
+        } else if(tableroActual.calcularResultado() === GANAN_NEGRAS) { //si ganan las negras
+            if (this.jugador === 1) { //si el jugador es negras entonces gana
                 reward = 100;
             } else { //pierde
                 reward = -100;
             } 
         } else{ //si la partida aún no termina
-            //asignaremos a las fichas coronadas un valor de 3 veces una ficha normal
             //FALTA EL REWARD DE TENER MÁS PIEZAS HACIA LOS LATERALES Y EL DE TENER MÁS PIEZAS CUBRIENDOSE
-            if (this.jugador == 2) { //si el jugador es blanco
-                reward += this.rewardPiezasBlancas(tableroActual);
+            if (this.jugador === 2) { //si el jugador es blanco
+                reward = this.rewardPiezasBlancas(tableroActual);
             } else { //si el jugador es negro
-                reward += this.rewardPiezasNegras(tableroActual);
+                reward = this.rewardPiezasNegras(tableroActual);
             }
             //reward += this.rewardPosBordes() +this.rewardCubrirAliado() +this.soplo();
         }
@@ -103,14 +119,15 @@ class MinimaxPodaAlfaBeta {
     }
 
     //subrutinas reward
+        //asignaremos el mismo valor a comer que a coronar
     rewardPiezasBlancas(tableroActual){
         return (tableroActual.cantPeonesBlancos - tableroActual.cantPeonesNegros) +
-        3*(tableroActual.cantDamasBlancas-tableroActual.cantDamasNegras);
+        (tableroActual.cantDamasBlancas-tableroActual.cantDamasNegras);
     }
 
     rewardPiezasNegras(tableroActual){
         return (tableroActual.cantPeonesNegros - tableroActual.cantPeonesBlancos) +
-        3*(tableroActual.cantDamasNegras-tableroActual.cantDamasBlancas);
+        (tableroActual.cantDamasNegras-tableroActual.cantDamasBlancas);
     }
 
     //funciones extra
