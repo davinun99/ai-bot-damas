d3ddfcd3a1f9c81a6ffe1a429808c4bcbf539014
matiaslopezs <matigls2@fpuna.edu.py>
2021-09-20 11:07:16 -0400

    nueva implementación de minimax con Poda obteniendo solo los movimientos de la rama min más alta

--- src/classes/MinimaxPodaAlfaBeta.js
+++ src/classes/MinimaxPodaAlfaBeta.js
@@ -7,7 +7,7 @@ class MinimaxPodaAlfaBeta {
     profundidadMax = 1; //cantidad de niveles del árbol que como máximo bajará
     jugador = 1; //asumo que jugador es blancas (2)
     rival = 2; //asumo que es negras (1) el rival
-    movimientoElegido = {};
+    //movimientoElegido = {};
 
     constructor( jugador = 1, N, tablero ){
         this.jugador = jugador;
@@ -22,14 +22,17 @@ class MinimaxPodaAlfaBeta {
         } else if(this.tableroActual.getAllJugadas(this.jugador).length === 0){ //si el jugador ya no tiene movimientos posibles pierde
             console.log("minimax con poda ha perdido");
         }else{
-            this.movimientoElegido = {}; //hacemos vacío el movimiento elegido
-            this.maxValue(this.tableroActual,this.profundidadMax,this.alfa,this.beta);
-            
-            let {movimiento, puedeCapturar, fichasCapturadas, ficha} = this.movimientoElegido; //desarmamos el movimiento
+
+            // console.log('cant mov rival '+this.tableroActual.getAllJugadas(this.rival));
+            // console.log('cant mov minim '+this.tableroActual.getAllJugadas(this.jugador));
+
+            //this.movimientoElegido = {}; //hacemos vacío el movimiento elegido
+            let movimientoElegido = this.decisionMinimaxAlfaBeta(this.tableroActual,this.profundidadMax);
+            let {movimiento, puedeCapturar, fichasCapturadas, ficha} = movimientoElegido; //desarmamos el movimiento
             let [movFila, movColumna] = movimiento; //ver valores del movimiento
             console.log('mov elegido: ['+movFila+']['+movColumna+']');
 
-            this.tableroActual.jugar(this.movimientoElegido); //jugamos con el movimiento elegido que fue cargado en maxValue
+            this.tableroActual.jugar(movimientoElegido); //jugamos con el movimiento elegido que fue cargado en maxValue
         }
     }
     jugarVsRandom(){
@@ -45,8 +48,8 @@ class MinimaxPodaAlfaBeta {
             }else{
                 this.tableroActual.dibujarTablero();
 
-                console.log('cant mov rival '+this.tableroActual.getAllJugadas(this.rival));
-                console.log('cant mov minim '+this.tableroActual.getAllJugadas(this.jugador));
+                // console.log('cant mov rival '+this.tableroActual.getAllJugadas(this.rival));
+                // console.log('cant mov minim '+this.tableroActual.getAllJugadas(this.jugador));
 
                 //movimiento del rival
                 console.log(index+") cantidad de fichas: "+this.tableroActual.cantFichasBlancas+" blancas y "+this.tableroActual.cantFichasNegras+" negras"); //mostramos al rival sus jugadas disponibles
@@ -55,70 +58,132 @@ class MinimaxPodaAlfaBeta {
                 console.log('movimiento de rival');
                 this.tableroActual.dibujarTablero();
                 //movimiento del minimax
-                this.movimientoElegido = {}; //hacemos vacío el movimiento elegido
-                console.log("print minimax: "+this.maxValue(this.tableroActual,this.profundidadMax,this.alfa,this.beta));
+                // this.movimientoElegido = {}; //hacemos vacío el movimiento elegido
+                // console.log("print minimax: "+this.maxValue(this.tableroActual,this.profundidadMax,this.alfa,this.beta));
                 //console.log("juego: "+this.tableroActual.calcularResultado());
-                let {movimiento, puedeCapturar, fichasCapturadas, ficha} = this.movimientoElegido; //desarmamos el movimiento
+                
+                let movimientoElegido = this.decisionMinimaxAlfaBeta(this.tableroActual,this.profundidadMax);
+                let {movimiento, puedeCapturar, fichasCapturadas, ficha} = movimientoElegido; //desarmamos el movimiento
                 let [movFila, movColumna] = movimiento; //ver valores del movimiento
                 console.log('mov elegido: ['+movFila+']['+movColumna+']');
-                this.tableroActual.jugar(this.movimientoElegido); //jugamos con el movimiento elegido que fue cargado en maxValue
+                this.tableroActual.jugar(movimientoElegido); //jugamos con el movimiento elegido que fue cargado en maxValue
             }
         }
         this.tableroActual.dibujarTablero();
     }
 
+    decisionMinimaxAlfaBeta(tableroActual,profundidadMax){ //función que llama a las funciones minimax y elige el siguiente movimiento
+        //let tableroSimulado = new Tablero();
+        //primero obtenemos el mejor reward
+        let rewardMax = this.alfa;
+        rewardMax = this.maxValue(tableroActual,profundidadMax,this.alfa,this.beta);
+        console.log(rewardMax);
+        //luego vemos todos los posibles movimientos para el tablero actual
+        let listaMejoresMovimientos = [];  
+        let listaMovimientosCaptura = []; 
+        let mejorMovElegido = null;
+        let mejorMovCaptura = null;
+        for (const movimiento of tableroActual.getAllJugadas(this.jugador)) {
+            const tableroSimulado = new Tablero(tableroActual.clonarTablero());
+            // tableroSimulado.table = tableroActual.clonarTablero();
+            tableroSimulado.jugar(movimiento);
+            //console.log('cant mov minim '+this.tableroActual.getAllJugadas(this.jugador).length);
+            let rewardMovimiento = this.minValue(tableroSimulado,profundidadMax-1,this.alfa,this.beta); //simulamos la jugada para obtener el reward a partir de cada mov
+            console.log('rewards posibles; '+rewardMovimiento);
+            if (rewardMovimiento === rewardMax) { //si el reward del movimiento es igual al reward máximo cargamos en la lista de mejores movimientos
+            // if (movimiento.reward === rewardMax) { seria bueno implementar así
+                mejorMovElegido = movimiento;
+                listaMejoresMovimientos.push(movimiento);
+                if (movimiento.puedeCapturar === true) { //verificamos además si hay movimientos de captura
+                    listaMovimientosCaptura.push(movimiento);
+                    mejorMovCaptura = mejorMovElegido;
+                }
+            }
+        }
+        //luego entre los mejores movimientos con el mismo reward elegimos uno al azar.  
+        let elegidoIndex = -1;
+        if (listaMejoresMovimientos.length > 1) { //si hay varios elementos en la lista elegir uno al azar
+            if (listaMovimientosCaptura.length > 1) {    //si hay más de un elemento de captura, elegimos al azar entre ellos
+                elegidoIndex = Math.floor(Math.random() * (listaMovimientosCaptura.length - 1));
+                mejorMovElegido = listaMovimientosCaptura[elegidoIndex];
+                console.log('existen varias opciones de captura');
+            }else if(listaMovimientosCaptura.length === 1){ //si hay solo un movimiento de captura, elegimos ese
+                mejorMovElegido = mejorMovCaptura;
+                console.log('existe una opción de captura');
+            }else{ //si no hay movimientos de captura elegir entre todos al azar
+                elegidoIndex = Math.floor(Math.random() * (listaMejoresMovimientos.length - 1));
+                mejorMovElegido = listaMejoresMovimientos[elegidoIndex];
+                console.log('no existen opciones de captura');
+            }
+        }      
+        console.log(JSON.stringify(mejorMovElegido));
+        return mejorMovElegido;
+    }
+
     //funciones minimax
     maxValue(tableroActual,profundidadMax,alfa,beta){ //función max de minimax
-        let tableroSimulado = new Tablero();
+        // let tableroSimulado = new Tablero();
+        let max = this.alfa;
         //tablero que simulará la jugada y la pasará al siguiente elemento
         if (profundidadMax <=0 || tableroActual.calcularResultado() !== JUEGO_INCONCLUSO){
             //cutOff Test: Si ya se llegó al valor de corte o si la partida ya termina
             return this.rewardJugada(tableroActual);
         }
-        let movElegido = [];
-        for (let movimiento of tableroActual.getAllJugadas(this.jugador)) { 
-            tableroSimulado.table = tableroActual.clonarTablero();
+        //let movElegido = [];
+        for (let movimiento of tableroActual.getAllJugadas(this.jugador)) {
+            
+            const tableroSimulado = new Tablero(tableroActual.clonarTablero()); 
+            // tableroSimulado.table = tableroActual.clonarTablero();
             //Jugamos cada movimiento y lo pasamos como parámetro al siguiente nivel del arbol
             tableroSimulado.jugar(movimiento);
             let posibleMov = this.minValue(tableroSimulado,profundidadMax-1,alfa,beta);
-
-            if (posibleMov >= beta) { //poda
-                console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' podado');
+            if(posibleMov > max){
+                max = posibleMov;
+            }
+            if (max >= beta) { //poda
+                // console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' podado');
                 //return beta;
                 break;
             }
-            if ( posibleMov >= alfa ) {
-                alfa = posibleMov;
-                movElegido = movimiento; //cargamos el movimiento en la variable global
+            if ( max >= alfa ) {
+                alfa = max;
+                //movElegido = movimiento; //cargamos el movimiento en la variable global
                 //hacemos lo de movElegido solo en alfa porque alfa son las jugadas de nuestro algoritmo y beta del rival
             }
             
-            this.movimientoElegido = movElegido;
+            //this.movimientoElegido = movElegido;
         }
-        console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' valor(alfa):'+ alfa+ ' mov: '+JSON.stringify(movElegido));
+        //console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' valor(alfa):'+ alfa);
         return alfa;
     }
 
 
     minValue(tableroActual,profundidadMax,alfa,beta){ //función min de minimax
-        let tableroSimulado = new Tablero();
+        // let tableroSimulado = new Tablero();
+        let min = this.beta;
         if (profundidadMax <=0  || tableroActual.calcularResultado() !== JUEGO_INCONCLUSO){ //cutOff Test
+            //console.log('entra aqui (min):'+ tableroActual.calcularResultado());
             return this.rewardJugada(tableroActual);
         }
         for (let movimiento of tableroActual.getAllJugadas(this.rival)) {  
-            tableroSimulado.table = tableroActual.clonarTablero();
+            
+            const tableroSimulado = new Tablero(tableroActual.clonarTablero());
+            // tableroSimulado.table = tableroActual.clonarTablero();
             tableroSimulado.jugar(movimiento);
             let posibleMov = this.maxValue(tableroSimulado,profundidadMax-1,alfa,beta);
-            if (alfa >= posibleMov) { //poda
-                console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' podado');
+            if(posibleMov < min){
+                min = posibleMov;
+            }
+            if (alfa >= min) { //poda
+                //console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' podado');
                 //return alfa;
                 break;
             }
-            if (posibleMov < beta) {
-                beta = posibleMov;
+            if (min < beta) {
+                beta = min;
             }
         }
-        console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' valor(beta):'+ beta);
+        //console.log('['+this.jugador+']profundidad: '+(4-profundidadMax)+' valor(beta):'+ beta);
         return beta;
     }
 
